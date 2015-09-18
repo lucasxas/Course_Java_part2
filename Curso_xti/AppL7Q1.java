15d89e21a5b9ccf7c309c09bacc710b40cbee554
Lucas Soares <lucas.soares@trademe.co.nz>
2015-09-18 15:53:27 +1200

    Ajeitando a formatação

--- Curso_xti/AppL7Q1.java
+++ Curso_xti/AppL7Q1.java
@@ -6,50 +6,42 @@
 import java.util.Scanner;
 
 public class AppL7Q1 {
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        
-        int nuVetor = 2;
-        
-        //invocando a classe L7Q1
-        
-        L7Q1 agenda = new L7Q1();
-        
-            agenda.mostra();
-            
-            //Entrando com os dados no laço ...
-            
-                for(int x = 0; x < agenda.nuVetor; ++x){
-                    //Entrando com o nome no vetor
-                    System.out.println("Entre com o nome na posição -"+(x+1));
-                        agenda.nome[x] = input.nextLine();
-                        
-                        //entrando com o endereço 
-                        System.out.println("Entre com o endereço na posição -"+(x+1));
-                            agenda.endereço[x] = input.nextLine();
-                            
-                            //Entrando com o telefone ...
-                            System.out.println("Entre com o telefone na posição -"+(x+1));
-                                agenda.telefone[x] = input.nextLong();
-                }
-                
-                                        
-                
-                
-                
-                                            //exibindo os resultados...
-                                                    
-                                                    for(int x = 0; x < agenda.nuVetor; ++x){
-                                                        // exibindo...
-                                                            System.out.println("Nome :"+agenda.nome[x]+"    "+"Endereço :"+agenda.endereço[x]+" "+"Telefone :"+agenda.telefone[x]);
-                                                            
-                                                    }
-        
-        
-        
-        
-        
-        
-    }
-    
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int nuVetor = 2;
+
+		// invocando a classe L7Q1
+
+		L7Q1 agenda = new L7Q1();
+
+		agenda.mostra();
+
+		// Entrando com os dados no laço ...
+
+		for (int x = 0; x < agenda.nuVetor; ++x) {
+			// Entrando com o nome no vetor
+			System.out.println("Entre com o nome na posição -" + (x + 1));
+			agenda.nome[x] = input.nextLine();
+
+			// entrando com o endereço
+			System.out.println("Entre com o endereço na posição -" + (x + 1));
+			agenda.endereço[x] = input.nextLine();
+
+			// Entrando com o telefone ...
+			System.out.println("Entre com o telefone na posição -" + (x + 1));
+			agenda.telefone[x] = input.nextLong();
+		}
+
+		// exibindo os resultados...
+
+		for (int x = 0; x < agenda.nuVetor; ++x) {
+			// exibindo...
+			System.out.println("Nome :" + agenda.nome[x] + "    " + "Endereço :" + agenda.endereço[x] + " "
+					+ "Telefone :" + agenda.telefone[x]);
+
+		}
+
+	}
+
 }
